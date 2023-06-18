a2acfbdee0c7ed086742af08c6dc0b17c70efb87
lev2454 <lewartamonov@gmail.com>
2023-06-18 23:39:10 +0200

    change print function

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ from threading import Thread
 from socket import socket, AF_INET, SOCK_DGRAM, SOCK_STREAM
 
 
+def print(*args, **kwargs):
+    print('\r', end='')
+    print(*args, end='\n> ')
+
+
 def send_command(cmd: bytes, ip: str, wait=True):
     try:
         with socket(AF_INET, SOCK_STREAM) as s:
